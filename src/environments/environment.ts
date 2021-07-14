1978ea54639116a03dd25eac6401229c2e1c0861
James Pound <jpound@webmd.net>
2021-07-13 21:00:57 -0400

    Updated firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,16 @@
 
 export const environment = {
 	production: false,
+	firebaseConfig: {
+		apiKey: "AIzaSyD7uvl2RmKA3ylBBKMZohOi9JOKlfr6Vag",
+		authDomain: "pos-mock-database.firebaseapp.com",
+		databaseURL: "https://pos-mock-database-default-rtdb.firebaseio.com",
+		projectId: "pos-mock-database",
+		storageBucket: "pos-mock-database.appspot.com",
+		messagingSenderId: "185556480229",
+		appId: "1:185556480229:web:32245eef5bc555f496e40c",
+		measurementId: "G-EGDKV6XMCS"
+	}
 };
 
 /*
