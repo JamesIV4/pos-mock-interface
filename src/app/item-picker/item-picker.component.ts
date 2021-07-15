9de4bc3060e8e47fee856db403267859dee29276
James Pound <jpound@webmd.net>
2021-07-15 09:53:25 -0400

    Reduced clutter

--- src/app/item-picker/item-picker.component.ts
+++ src/app/item-picker/item-picker.component.ts
@@ -25,18 +25,14 @@ export class ItemPickerComponent implements OnInit {
 			event.target.classList.remove(className); // Remove 'closed' class
 			event.target.nextSibling.style.height = event.target.nextSibling.scrollHeight + 'px';
 
-			// After animation is done playing, remove height to keep responsive behavior of list
-			setTimeout(function animCleanup() {
-				event.target.nextSibling.style.removeProperty('height');
-			}, 500);
+			// After animation is done playing, remove height property to keep responsive behavior of list
+			setTimeout(() => {event.target.nextSibling.style.removeProperty('height')}, 500);
 		} else {
 			event.target.classList.add(className); // Add 'closed' class
 			
 			// Before animation plays, set the height property so the transition will play properly
 			event.target.nextSibling.style.height = event.target.nextSibling.scrollHeight + 'px';
-			setTimeout(function beginAnim() {
-				event.target.nextSibling.style.height = '0px';
-			}, 1);
+			setTimeout(() => {event.target.nextSibling.style.height = '0px'}, 1);
 		}
 	}
 }
