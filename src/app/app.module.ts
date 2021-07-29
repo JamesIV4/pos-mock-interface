2b1982038da22c7bd3403aefbb65d93f941addce
James Pound <jpound@webmd.net>
2021-07-29 10:58:35 -0400

    Fixed child page 404 issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { environment } from 'src/environments/environment';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { MatIconModule } from '@angular/material/icon';
-import { RouterModule, Routes } from '@angular/router'
-
+//import { AngularFireModule } from '@angular/fire';
+//import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AppComponent } from './app.component';
-import { PageItemPickerComponent } from './page-item-picker/page-item-picker.component';
-import { OrderStatusComponent } from './order-status/order-status.component';
-import { PageOrderListComponent } from './page-order-list/page-order-list.component';
+import { BrowserModule } from '@angular/platform-browser';
+//import { environment } from 'src/environments/environment';
 import { ListDisplayComponent } from './list-display/list-display.component';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { OrderStatusComponent } from './order-status/order-status.component';
 import { PageFinalizeOrderComponent } from './page-finalize-order/page-finalize-order.component';
+import { PageItemPickerComponent } from './page-item-picker/page-item-picker.component';
+import { PageOrderListComponent } from './page-order-list/page-order-list.component';
+import { RouterModule, Routes } from '@angular/router'
 
 const routes: Routes = [
 	{ path: '', component: PageItemPickerComponent },
@@ -30,10 +29,12 @@ const routes: Routes = [
 	],
 	imports: [
 		BrowserModule,
-		AngularFireModule.initializeApp(environment.firebaseConfig),
-		AngularFirestoreModule,
+		//AngularFireModule.initializeApp(environment.firebaseConfig),
+		//AngularFirestoreModule,
 		MatIconModule,
-		RouterModule.forRoot(routes),
+		RouterModule.forRoot(routes, {
+			useHash: false
+		}),
 	],
 	providers: [],
 	bootstrap: [AppComponent]
